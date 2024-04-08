4aafa4dc182abee4d6adc1c569deeb9980b594da
SonnyUplavan <sonny-issara.uplavan.1@ens.etsmtl.ca>
2024-04-08 08:17:15 -0400

    removing DB unitTest

--- test/test.py
+++ test/test.py
@@ -32,41 +32,7 @@ class TestApp(unittest.TestCase):
             "postgresql://mocked_user:mocked_password@mocked_host:5432/mock_db",
         )
     
-    def test_insert_value_into_database(self):
-        # Define your database connection parameters
-        DB_CONNECTION_PARAMS = {
-            "dbname": "db02eq12",
-            "user": "user02eq12",
-            "password": "E84YDXF2l5P4FkFG",
-            "host": "157.230.69.113",
-            "port": "5432",
-        }
-
-        # Establish a connection to the database
-        try:
-            conn = psycopg2.connect(**DB_CONNECTION_PARAMS)
-        except psycopg2.Error as e:
-            self.fail(f"Error connecting to the database: {e}")
-
-        # Define the data you want to insert into the database
-        data = ("2024-04-04 12:00:00", 25.0, "None")
-
-        # Define the SQL statement to insert data into the table
-        insert_query = """
-            INSERT INTO hvac_events (timestamp_event, temperature, event_type)
-            VALUES (%s, %s, %s)
-        """
-
-        # Insert data into the database
-        try:
-            with conn.cursor() as cursor:
-                cursor.execute(insert_query, data)
-                conn.commit()
-        except psycopg2.Error as e:
-            self.fail(f"Error inserting data into the database: {e}")
-        finally:
-            # Close the database connection
-            conn.close()
+   
 
 if __name__ == '__main__':
     unittest.main()
